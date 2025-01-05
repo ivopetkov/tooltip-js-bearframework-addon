ddc5dd1f447709f334b3cd83557bbe0372c8f8c5
Ivo Petkov <ivo@ivopetkov.com>
2025-01-05 13:44:40 +0200

    Add PHP 8.4 support. Remove PHP 7.* support.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $app = App::get();
 $context = $app->contexts->get(__DIR__);
 
 $app->clientPackages
-    ->add('tooltip', function (IvoPetkov\BearFrameworkAddons\ClientPackage $package) use ($context) {
+    ->add('tooltip', function (IvoPetkov\BearFrameworkAddons\ClientPackage $package) use ($context): void {
         $package->addJSCode(include $context->dir . '/assets/tooltip.min.js.php');
         //$package->addJSCode(file_get_contents($context->dir . '/dev/tooltip.js'));
         $package->embedPackage('escapeKey');
